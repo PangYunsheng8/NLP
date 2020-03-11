44c0813c0393f2580058a5525f3fb67a3581bcf4
YunshengPang <434497112@qq.com>
2020-03-12 00:06:48 +0800

    modify edit distance

--- preprocessing/Edit_Distance.py
+++ preprocessing/Edit_Distance.py
@@ -8,19 +8,19 @@ class EditDistance:
     def compute_distance(self, word1: str, word2: str) -> int:
         m = len(word1)
         n = len(word2)
-        edit = [[0 for _ in range(n)] for _ in range(m)]
+        edit = [[0 for _ in range(n+1)] for _ in range(m+1)]
 
-        for i in range(m):
-            for j in range(n):
+        for i in range(m+1):
+            for j in range(n+1):
                 if min(i, j) == 0:
                     edit[i][j] = max(i, j)
                 else:
-                    flag = 0 if word1[i] == word2[j] else 1
+                    flag = 0 if word1[i-1] == word2[j-1] else 1
                     edit[i][j] = min(edit[i-1][j] + 1, edit[i][j-1] + 1, edit[i-1][j-1] + flag)
         return edit[-1][-1]
 
 
 if __name__ == '__main__':
     ed = EditDistance()
-    ed_value = ed.compute_distance('bitch', 'batches')
+    ed_value = ed.compute_distance('horse', 'ros')
     print(ed_value)
